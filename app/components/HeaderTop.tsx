f6758a201e1e4fc0d95f7ecf25f2addbc05bb0d6
Francis Regala <pantet008@gmail.com>
2023-05-23 23:09:08 +0800

    Headertop responsivenes

--- app/components/HeaderTop.tsx
+++ app/components/HeaderTop.tsx
@@ -9,7 +9,7 @@ const HeaderTop: FC<HeaderTopProps> = () => (
     className={`flex py-2 backdrop-blur-lg top-0 justify-between leading-none gap-4 antialiased transition shadow-sm`}
     style={{ background: '#B2967D', color: '#F0F0F0', fontFamily: 'Unna', fontStyle: 'normal', fontWeight: '700', fontSize: '24px', lineHeight: '32px', }}
   >
-    <a className="font-bold w-full text-center hidden md:block md:text-xl lg:text-2xl" href="/">
+    <a className="font-bold w-full text-center hidden md:block md:text-sm lg:text-md 2xl:text-lg" href="/">
       TAX FREE + FREE SHIPPING WITHIN NORWAY / WORLDWIDE SHIPPING
     </a>
   </div>

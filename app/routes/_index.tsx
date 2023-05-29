0e35f89c0741f88d983f0b42273e4c060667d096
Francis Regala <pantet008@gmail.com>
2023-05-29 21:30:44 +0800

    Finalized commit

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -42,7 +42,7 @@ export default function Index() {
           </div>
         </div>
         {/* Step Up Your Style with Kayani - MD */}
-        <div className="hidden md:block py-[60px]">
+        <div className="hidden md:block py-[60px] 2xl:py-[80px]">
           <div className="grid grid-cols-2 gap-8 px-[80px] lg:px-[120px] xl:px-[200px]">
             <div className="grid grid-cols-1 gap-2 content-center w-11/12">
               <div className="text-4xl lg:text-4xl xl:text-6xl 2xl:text-7xl">
@@ -75,14 +75,14 @@ export default function Index() {
         </div>
 
         {/* Shop Bestsellers */}
-        <div className="md:grid md:grid-cols-3 md:gap-4 md:px-6 md:py-8 py-4 my-4">
+        <div className="md:grid md:grid-cols-3 md:gap-4 py-[60px] 2xl:py-[80px] md:px-[80px] lg:px-[120px] xl:px-[200px]">
           <div className="grid grid-cols-1 gap-4">
             <div className="girl1 h-[50vh] md:h-[39vh] lg:h-[50vh] xl:h-[75vh]">
               <div className="relative h-full w-full">
                 <div className="absolute inset-x-0 bottom-0 h-26 mb-8">
                   <div className="grid grid-cols-1 content-center text-center">
                     <span className="grid grid-cols-1 text-white text-4xl mb-4 md:text-2xl md:mb-2 xl:text-4xl 2xl:text-5xl">Bestsellers</span>
-                    <button className="grid grid-cols-1 w-[208px] text-xl md:text-sm md:w-[160px] xl:w-[208px] xl:text-xl 2xl:w-[308px] 2xl:text-2xl p-2 bg-white hover:bg-gray-100 text-gray-800 font-semibold border border-gray-400 rounded shadow my-2 place-self-center">
+                    <button className="grid grid-cols-1 w-[208px] text-xl md:text-sm md:w-[160px] xl:w-[208px] xl:text-xl 2xl:w-[308px] 2xl:text-2xl p-2 bg-white hover:bg-gray-100 text-gray-800 font-semibold shadow my-2 place-self-center">
                       Shop Bestsellers
                     </button>
                   </div>
@@ -96,7 +96,7 @@ export default function Index() {
                 <div className="absolute inset-x-0 bottom-0 h-26 mb-8">
                   <div className="grid grid-cols-1 content-center text-center">
                   <span className="grid grid-cols-1 text-white text-4xl mb-4 md:text-2xl md:mb-2 xl:text-4xl 2xl:text-5xl">New Arrivals</span>
-                  <button className="grid grid-cols-1 w-[208px] text-xl md:text-sm md:w-[160px] xl:w-[208px] xl:text-xl 2xl:w-[308px] 2xl:text-2xl p-2 bg-white hover:bg-gray-100 text-gray-800 font-semibold border border-gray-400 rounded shadow my-2 place-self-center">
+                  <button className="grid grid-cols-1 w-[208px] text-xl md:text-sm md:w-[160px] xl:w-[208px] xl:text-xl 2xl:w-[308px] 2xl:text-2xl p-2 bg-white hover:bg-gray-100 text-gray-800 font-semibold shadow my-2 place-self-center">
                       Shop Arrivals
                     </button>
                   </div>
@@ -110,7 +110,7 @@ export default function Index() {
                 <div className="absolute inset-x-0 bottom-0 h-26 mb-8">
                   <div className="grid grid-cols-1 content-center text-center">
                   <span className="grid grid-cols-1 text-white text-4xl mb-4 md:text-2xl md:mb-2 xl:text-4xl 2xl:text-5xl">Dress Collection</span>
-                  <button className="grid grid-cols-1 w-[208px] text-xl md:text-sm md:w-[160px] xl:w-[208px] xl:text-xl 2xl:w-[308px] 2xl:text-2xl p-2 bg-white hover:bg-gray-100 text-gray-800 font-semibold border border-gray-400 rounded shadow my-2 place-self-center">
+                  <button className="grid grid-cols-1 w-[208px] text-xl md:text-sm md:w-[160px] xl:w-[208px] xl:text-xl 2xl:w-[308px] 2xl:text-2xl p-2 bg-white hover:bg-gray-100 text-gray-800 font-semibold shadow my-2 place-self-center">
                       Shop Dress
                     </button>
                   </div>
@@ -146,7 +146,7 @@ export default function Index() {
           </div>
         </div>
         {/* See What's Trending - MD */}
-        <div className="hidden md:block grid grid-cols-1 gap-4">
+        <div className="hidden md:block grid grid-cols-1 gap-4 py-[60px] 2xl:py-[80px]">
           <div className="girl h-[39vh] lg:h-[50vh] xl:h-[72vh] 2xl:h-[88vh] content-center">
             <div className="grid grid-cols-2 gap-4">
               <div className="grid grid-cols-1 gap-4 content-center">
@@ -206,7 +206,7 @@ export default function Index() {
         </div>
 
         {/* New Arrivals - MD */}
-        <div className="hidden md:block pt-8 mt-8 px-6" style={{ background: '#F9EDEB' }}>
+        <div className="hidden md:block px-6 py-[60px] 2xl:py-[80px]" style={{ background: '#F9EDEB' }}>
           <div className="grid grid-cols-2 gap-4">
             <div className="text-4xl 2xl:text-6xl pt-2">
               <span>Looks You'll Love</span>
@@ -237,15 +237,20 @@ export default function Index() {
         </div>
 
         {/* Fashion That Is Always Relevant */}
-        <div className="grid grid-cols-1 gap-4 pt-8 pb-2 mt-8 pl-6 pr-8 mr-8">
+        <div className="block md:hidden grid grid-cols-1 gap-4 pt-[60px] 2xl:pt-[80px] px-6 md:px-[80px] lg:px-[120px] xl:px-[200px]">
           <div className="text-4xl 2xl:text-6xl text-bold">
             Fashion That Is Always Relevant
           </div>
         </div>
-        <div className="grid grid-cols-2 gap-4 px-6 md:pb-6">
-          <div className="fashion-sm mt-8 h-[20vh] md:h-[44vh] lg:h-[60vh] xl:h-[86vh]"></div>
-          <div className="grid grid-cols-1 gap-4 content-end ">
-            <div className="grid grid-cols-1 gap-4">
+        <div className="grid grid-cols-2 gap-4 px-6 pb-[60px] 2xl:pb-[80px] md:py-[60px] md:px-[80px] lg:px-[120px] xl:px-[200px] ">
+          <div className="fashion-sm h-[20vh] md:h-[38vh] lg:h-[49vh] xl:h-[65vh] 2xl:h-[86vh] self-end"></div>
+          <div className="grid grid-cols-1 gap-4">
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-2">
+              <div className="hidden md:block col-span-2">
+                <div className="text-4xl lg:text-4xl xl:text-6xl 2xl:text-7xl text-bold">
+                  Fashion That Is Always Relevant
+                </div>
+              </div>
               <div className="flex flex-col">
                 <div>
                   <Link type="button" to="/shop"
@@ -260,32 +265,11 @@ export default function Index() {
                 </div>
               </div>
             </div>
-            <div className="grid grid-cols-2 gap-4">
-              <div className="girl-brown-sm h-[11.5vh] md:h-[27vh] lg:h-[37vh] xl:h-[54vh]"></div>
-              <div className="girl-green-sm h-[11.5vh] md:h-[27vh] lg:h-[37vh] xl:h-[54vh]"></div>
+            <div className="grid grid-cols-2 gap-4 content-end">
+              <div className="girl-brown-sm h-[11.5vh] md:h-[24vh] lg:h-[32vh] xl:h-[43vh] 2xl:h-[57vh]"></div>
+              <div className="girl-green-sm h-[11.5vh] md:h-[22vh] lg:h-[30vh] xl:h-[40vh] 2xl:h-[54vh]"></div>
             </div>
           </div>
-          {/* <div className="content-end">
-            <div className="grid grid-cols-1 gap-4">
-              <div className="flex flex-col">
-                <div>
-                  <Link type="button" to="/shop"
-                    className="font-medium float-right text-black border border-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium text-base px-4 text-center mb-2 dark:border-gray-600 dark:text-black dark:focus:ring-gray-800">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-12">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25" />
-                      </svg>
-                  </Link>
-                </div>
-                <div>
-                  <span className="float-right text-md">Shop Now</span>
-                </div>
-              </div>
-            </div>
-            <div className="grid grid-cols-2 gap-4 content-end">
-              <div className="girl-brown-sm h-[11.5vh] md:h-[27vh] lg:h-[37vh] xl:h-[502px]"></div>
-              <div className="girl-green-sm h-[11.5vh] md:h-[27vh] lg:h-[37vh] xl:h-[483px]"></div>
-            </div>
-          </div> */}
         </div>
 
         {/* Elevate your style game and shop Kayani's unique collection today - SM */}
@@ -320,13 +304,13 @@ export default function Index() {
         </div>
 
         {/* Elevate your style game and shop Kayani's unique collection today - MD */}
-        <div className="hidden md:block">
+        <div className="hidden md:block py-[60px] 2xl:py-[80px]">
           <div
               role="banner"
               className={`flex p-4 backdrop-blur-lg top-0 justify-between leading-none gap-4 antialiased transition shadow-sm`}
               style={{ background: '#B2967D', color: '#F0F0F0', fontFamily: 'Unna' }}
             >
-              <div className="grid grid-cols-2 gap-4 p-8 w-full">
+              <div className="grid grid-cols-2 gap-4 p-8 w-full py-[60px]">
                 <div className="grid grid-cols-1 gap-4 content-center px-4">
                   <div className="text-xl lg:text-2xl xl:text-6xl 2xl:text-7xl text-right">
                     <span>Elevate your style game and shop Kayani's unique collection today!</span>
@@ -353,185 +337,6 @@ export default function Index() {
           </div>
         </div>
       </div>
-
-      {/* <div className="hidden sm:block">
-        <div className="index1 pt-8">
-          <div className="px-200">
-            <div className="grid grid-cols-2 gap-4">
-              <div className="grid grid-cols-1 gap-4 content-center" style={{ height: '800px'}}>
-                <div className="landing-text">
-                  Step Up Your Style with Kayani
-                </div>
-                <div className="landing-text-small">
-                  Shop Kayani's Exclusive Collection for Unique and Trendy Clothing That Will Make You Stand Out
-                </div>
-                <div className="flex flex-col pt-8 mt-8" style={{ paddingRight: '80px' }}>
-                  <div>
-                    <button type="button"
-                      className="font-medium float-right text-black border border-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium text-base px-5 py-2.5 text-center mb-2 dark:border-gray-600 dark:text-black dark:focus:ring-gray-800">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
-                          <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25" />
-                        </svg>
-                    </button>
-                  </div>
-                  <div>
-                    <span className="float-right text-lg">Shop Now</span>
-                  </div>
-                </div>
-              </div>
-
-              <div className="grid grid-cols-1 gap-4 content-center" style={{ height: '800px'}}>
-                <div className="grid grid-cols-2 gap-4">
-                  <div className="image-half-circle"></div>
-                  <div className="image-half-circle1"></div>
-                </div>
-              </div>
-            </div>
-
-            <div className="grid grid-cols-3 gap-4" style={{ paddingTop: '100px', paddingBottom: '100px'}}>
-              <div className="grid grid-cols-1 gap-4">
-                <div className="girl1">
-                  <div className="relative h-full w-full">
-                    <div className="absolute inset-x-0 bottom-0 h-26 mb-8 pb-8">
-                      <div className="grid grid-cols-1 gap-4 content-center text-center">
-                        <span className="grid grid-cols-1 text-white text-2xl">Bestsellers</span>
-                        <button className="grid grid-cols-1 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow my-4"
-                          style={{ margin: '0px 90px' }}>
-                          Shop Bestsellers
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="grid grid-cols-1 gap-4">
-                <div className="girl2">
-                  <div className="relative h-full w-full">
-                    <div className="absolute inset-x-0 bottom-0 h-26 mb-8 pb-8">
-                      <div className="grid grid-cols-1 gap-4 content-center text-center">
-                        <span className="grid grid-cols-1 text-white text-2xl">New Arrivals</span>
-                        <button className="grid grid-cols-1 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow my-4"
-                          style={{ margin: '0px 90px' }}>
-                          Shop Arrivals
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="grid grid-cols-1 gap-4 content-center">
-                <div className="girl3">
-                  <div className="relative h-full w-full">
-                    <div className="absolute inset-x-0 bottom-0 h-26 mb-8 pb-8">
-                      <div className="grid grid-cols-1 gap-4 content-center text-center">
-                        <span className="grid grid-cols-1 text-white text-2xl">Dress Collection</span>
-                        <button className="grid grid-cols-1 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow my-4"
-                          style={{ margin: '0px 90px' }}>
-                          Shop Dress
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="grid grid-cols-1 gap-4">
-          <div className="girl">
-            <div className="grid grid-cols-2 gap-4">
-              <div className="grid grid-cols-1 gap-4 content-center" style={{ height: '800px'}}>
-                
-              </div>
-
-              <div className="grid grid-cols-1 gap-4 content-center" style={{ height: '800px'}}>
-                <div className="landing-text">
-                  See What's Trending
-                </div>
-                <div className="landing-text-small text-justify w-5/6">
-                  Check out KAYANI's must-have featured product! Designed with the modern woman in mind, it's a unique and eye-catching piece that combines trendiness with timelessness. Feel confident and powerful with this high-quality item, perfect for any occasion from a night out to a formal event.
-                </div>
-                <div className="flex flex-col pt-8 mt-8 pr-8 mr-8" style={{ paddingRight: '120px' }}>
-                  <div>
-                    <button type="button"
-                      className="font-medium float-right text-black border border-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium text-base px-5 py-2.5 text-center mb-2 dark:border-gray-600 dark:text-black dark:focus:ring-gray-800">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
-                          <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25" />
-                        </svg>
-                    </button>
-                  </div>
-                  <div>
-                    <span className="float-right text-lg">Shop Now</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="grid grid-cols-2 gap-4" style={{ padding: '80px 106px 10px 106px', background: '#F9EDEB' }}>
-          <div className="looks-love">
-            <span>Looks You'll Love</span>
-          </div>
-          <div className="float-right">
-            <div>
-              <button type="button"
-                className="font-medium float-right text-black border border-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium text-base px-5 py-2.5 text-center m-2 dark:border-gray-600 dark:text-black dark:focus:ring-gray-800">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
-                  </svg>
-              </button>
-              <button type="button"
-                className="font-medium float-right text-black border border-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium text-base px-5 py-2.5 text-center m-2 dark:border-gray-600 dark:text-black dark:focus:ring-gray-800">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
-                  </svg>
-              </button>
-            </div>
-          </div>
-        </div>
-        <div className="grid grid-cols-4 gap-4" style={{ padding: '4px 106px 100px 106px', background: '#F9EDEB' }}>
-          <div className="looks1"></div>
-          <div className="looks2"></div>
-          <div className="looks3"></div>
-          <div className="looks4"></div>
-        </div>
-
-        <div className="index2 pt-8">
-          <div className="px-200">
-            <div className="grid grid-cols-2 gap-4">
-              <div className="fashion"></div>
-              <div>
-                <div className="grid grid-cols-2 gap-2 pb-4">
-                  <div className="looks-love">
-                    <strong>Fashion That Is Always Relevant</strong>
-                  </div>
-                  <div>
-                    <div className="flex flex-col pt-8 mt-8 pr-8 mr-8" style={{ paddingRight: '120px' }}>
-                      <div>
-                        <button type="button"
-                          className="font-medium text-black border border-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium text-base px-5 py-2.5 text-center mb-2 dark:border-gray-600 dark:text-black dark:focus:ring-gray-800">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
-                              <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25" />
-                            </svg>
-                        </button>
-                      </div>
-                      <div>
-                        <span className="text-lg">Shop Now</span>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="grid grid-cols-2 gap-2">
-                  <div className="girl-brown"></div>
-                  <div className="girl-green"></div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div> */}
     </>
     );
 }
